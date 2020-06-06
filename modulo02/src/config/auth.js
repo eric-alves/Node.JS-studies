f82696a618d27fe54f6988b29b863350ef5fa66e
Eric dos Reis Alves <eralves01@hotmail.com>
2020-06-06 02:15:54 -0300

    Criacao da validacao dos dados utilizando o yup

---
+++ modulo02/src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret: 'umconjuntodecaracteresqualquer',
+    expiresIn: '7d',
+}
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,19 +1,21 @@
 const {Router} = require('express');
 const User = require('./app/models/User');
 const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
+
+const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Eric dos Reis Alves',
-        email: 'eralves02@hotmail.com',
-        password_hash: '123456',
-    });
-
-    res.json(user);
+    res.json({message: 'Projeto GoBarber'});
 })
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 module.exports = routes;
